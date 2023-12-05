941836c9089dc63f32112041b14d39a460cccae0
lyonsm <megan.lyons@ons.gov.uk>
2023-12-05 14:31:37 +0000

    feat - wip plots for av prices and func for mode

--- main.R
+++ main.R
@@ -1,6 +1,7 @@
 #### Load packages ------------------------------------------------------------
 library(dplyr)
-
+library(ggplot2)
+source("functions.R")
 
 #### Processing ---------------------------------------------------------------
 ames_raw <- read_csv("ames.csv") %>%
@@ -26,8 +27,43 @@ check_missing <- summarise(ames, across(everything(), ~ sum(is.na(.))))
 
 ames <- ames_raw %>%
   mutate(garage = ifelse(is.na(garage_type), "No garage", "Garage"),
-         has_pool = ifelse(pool_area == 0, "No pool", "Pool"))
-
+         has_pool = ifelse(pool_area == 0, "No pool", "Pool"),
+         year_group = cut(
+           year_built,
+           breaks = c(seq(1850, 2025, by = 25)),
+           labels = c("1850 to 1875",
+                      "1876 to 1900",
+                      "1901 to 1925",
+                      "1926 to 1950",
+                      "1951 to 1975",
+                      "1976 to 2000",
+                      "2001 to 2015")))
 
 
 #### Analysis -----------------------------------------------------------------
+
+av_price_grps <- ames %>%
+  group_by(year_group) %>%
+  summarise(mean = mean(sale_price),
+            mode = getmode(sale_price),
+            median = median(sale_price),
+            count = n())
+
+av_price <- ames %>%
+  group_by(year_built) %>%
+  summarise(mean = mean(sale_price),
+            mode = getmode(sale_price),
+            median = median(sale_price),
+            count = n())
+
+
+plot_grps <- ggplot(av_price_grps, aes(x = year_group, group = 1)) +
+  geom_line(aes(y = mean), colour = "red") +
+  geom_line(aes(y = mode), colour = "blue") +
+  geom_line(aes(y = median), colour = "orange")
+  
+  
+plot <- ggplot(av_price, aes(x = year_built, group = 1)) +
+  geom_line(aes(y = mean), colour = "red") +
+  geom_line(aes(y = mode), colour = "blue") +
+  geom_line(aes(y = median), colour = "orange")
